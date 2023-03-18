163cf8de4eaef5b0459ba75d8c1a5e0fa776cac8
elkevino6 <elkevino6@gmail.com>
2023-03-18 16:08:55 -0700

    added success/error messages to addstudent.js

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 function AddStudent() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
- 
 
   const [student, setStudent] = useState({
     name: '',
@@ -18,7 +17,6 @@ function AddStudent() {
     const data = {
       name: name,
       email: email
-      
     };
     
     axios.post('http://localhost:8080/student', data)
@@ -27,42 +25,49 @@ function AddStudent() {
         setStudent({
           name: name,
           email: email,
-          status_code: 0
+          statusCode: 201
         });
+        setName('');
+        setEmail('');
       })
       .catch((error) => {
         console.log(error);
         setStudent({
           name: name,
           email: email,
-          status_code: 0
+          statusCode: 400
         });
       });
   };
 
+  let message;
+  if (student.statusCode === 201) {
+    message = <p>Student added successfully</p>;
+  } else if (student.statusCode === 400) {
+    message = <p>Error adding student</p>;
+  }
+
   return (
-   <div>
-  <h2>Add a New Student</h2>
-  <form onSubmit={handleSubmit}>
     <div>
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <h2>Add a New Student</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <p>Name: {student.name}</p>
+        <p>Email: {student.email}</p>
+        {message}
+      </div>
     </div>
-    <div>
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-    </div>
-    <button type="submit">Submit</button>
-  </form>
-  <div>
-    <p>Name: {student.name}</p>
-    <p>Email: {student.email}</p>
-  </div>
-</div>
   );
 }
 
 export default AddStudent;
-
-
-
